1a12d632d96afa8f7f6bbbf83a02f49dec466e82
unknown <Jernimo>
2025-07-19 17:40:03 -0300

    se termina el modelo de los turnos

--- src/modelos/turnos.js
+++ src/modelos/turnos.js
@@ -1,11 +1,29 @@
 const mongoose = require("mongoose");
 
 const turnosSchema = new mongoose.Schema({
-  detalle: {},
-  veterinario: {},
-  mascota: {},
-  fecha: {},
-  hora: {},
+  detalle: {
+    type: String,
+    trim: true,
+    required: true,
+  },
+  veterinario: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  mascota: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  fecha: {
+    type: Date,
+    default: null,
+  },
+  hora: {
+    type: Date,
+    default: null,
+  },
 });
 
 const turnosModelo = mongoose.model("turnos", turnosSchema);
