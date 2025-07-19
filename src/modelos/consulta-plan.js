35ec7baef2ccba7853a96ec713ddc0faeff8e5a8
unknown <Jernimo>
2025-07-19 17:52:45 -0300

    se termina de armar el modelo de la consulta de los planes

--- src/modelos/consulta-plan.js
+++ src/modelos/consulta-plan.js
@@ -1,11 +1,34 @@
 const mongoose = require("mongoose");
 
 const consultaSchema = new mongoose.Schema({
-  tipoPlan: {},
-  nombreUsuario: {},
-  emailUsuario: {},
-  mensaje: {},
-  fechaCreación: {},
+  tipoPlan: {
+    type: String,
+    required: true,
+    trim: true,
+    enum: ["Primeros pasos", "Madurando", "Adultos"],
+  },
+  nombreUsuario: {
+    type: String,
+    required: true,
+    trim: true,
+    unique: true,
+  },
+  emailUsuario: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  mensaje: {
+    type: String,
+    required: true,
+    trim: true,
+    minlength,
+    maxlenght,
+  },
+  fechaCreación: {
+    type: Date,
+    default: Date.now(),
+  },
 });
 
 const modeloConsulta = mongoose.model("consultaPLan", consultaSchema);
