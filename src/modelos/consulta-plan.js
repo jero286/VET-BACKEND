07ce14881a825a5a0b4f82001dda4b3aafbdd5ab
unknown <Jernimo>
2025-07-19 17:47:03 -0300

    se termina la estructura básica del modelo

--- src/modelos/consulta-plan.js
+++ src/modelos/consulta-plan.js
@@ -1,5 +1,13 @@
 const mongoose = require("mongoose");
 
 const consultaSchema = new mongoose.Schema({
-  
-})
+  tipoPlan: {},
+  nombreUsuario: {},
+  emailUsuario: {},
+  mensaje: {},
+  fechaCreación: {},
+});
+
+const modeloConsulta = mongoose.model("consultaPLan", consultaSchema);
+
+module.exports = modeloConsulta;
