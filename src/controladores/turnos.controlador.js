c03bbdc7868fc0667375b8fca7d7278baa900b5d
unknown <Jernimo>
2025-07-21 16:45:34 -0300

    funcion para eliminar turnos creada

--- src/controladores/turnos.controlador.js
+++ src/controladores/turnos.controlador.js
@@ -1,7 +1,13 @@
 const obtenerTurnoPorId = async (req, res) => {
   try {
   } catch (error) {
-    res.status(404).json({ msg: "Error al obtener producto:", error });
+    res.status(404).json({ msg: "Turno no encontrado", error });
   }
 };
 
+const eliminarTurno = async (req, res) => {
+  try {
+  } catch (error) {
+    res.status(404).json({ msg: "No se puede borrar un turno inexistente" });
+  }
+};
