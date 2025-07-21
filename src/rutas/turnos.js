b69f3d404104f8dc3b6d9b8616416100d188d3c2
unknown <Jernimo>
2025-07-20 23:44:08 -0300

    fin de crud de turnos

---
+++ src/rutas/turnos.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const router = express.Router();
+
+router.get("/:id");
+router.post("/");
+router.put("/:id");
+router.delete("/:id");
+
+module.exports = router;
+
+//CRUD TURNOS
