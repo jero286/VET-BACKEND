03439a08e330d67620917a0e02fa2365407075af
Magali Canelo <maguidiaz1e@gmail.com>
2025-07-23 11:18:29 -0300

    correccion de nombre

--- src/rutas/carrito.js
+++ src/rutas/carrito.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const carritoController = require('../controllers/carrito.controller');
+const carritoController = require('../controladores/carrito.controladores');
 
 // Ruta para agregar producto al carrito
 router.post('/agregar', carritoController.agregarProducto);
