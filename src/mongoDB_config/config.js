988922b3c85715dce48045eec3978e73143f8ffd
Magali Canelo <maguidiaz1e@gmail.com>
2025-07-24 11:28:06 -0300

    movi .env

--- src/mongoDB_config/config.js
+++ src/mongoDB_config/config.js
@@ -2,6 +2,6 @@ const mongoose = require("mongoose");
 require("dotenv").config();
 
 mongoose
-  .connect(process.env.MONGO_URI)
+  .connect(process.env.MONGO_PASS)
   .then(() => console.log("Base de datos conectada"))
   .catch((error) => console.log(error));
