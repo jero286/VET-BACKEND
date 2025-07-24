cb5a101742020379e05184f97abfaa88128c2ba0
JLM224 <jorgemedina051001@gmail.com>
2025-07-24 17:41:46 -0300

    se crean los controladores de mascotas y se las relaciona al usuario

--- src/servicios/mascotas.servicios.js
+++ src/servicios/mascotas.servicios.js
@@ -1,40 +1,43 @@
 const MascotasModelo = require("../modelos/mascotas.modelo")
 
 const obtenerTodasLasMascotasServicios = async () => {
-    const mascotas = await MascotasModelo.find()
+    const mascotas = await MascotasModelo.find({usuario: idUsuario})
     return{
         mascotas,
         statuCode: 200
     }
 }
 
-const obtenerMascotaPorIdServicios = async (idMascota) => {
-    const mascota = await MascotasModelo.findOne({_id: idMascota})
+const obtenerMascotaPorIdServicios = async (idMascota, idUsuario) => {
+    const mascota = await MascotasModelo.findOne({_id: idMascota, usuario: idUsuario})
     return{
         mascota,
         statusCode: 200
     }
 }
 
-const crearNuevaMascotaServicios = async (body) => {
-    const nuevaMascota = new MascotasModelo(body)
+const crearNuevaMascotaServicios = async (body, idUsuario) => {
+    const bodyConIdUsuario = {...body,
+        usuario: idUsuario
+    }
+    const nuevaMascota = new MascotasModelo(bodyConIdUsuario)
     await nuevaMascota.save()
     return{
         msg:"Mascota creada",
-        statuCode: 201
+        statusCode: 201
     }
 }
 
 const actualizarMascotaPorIdServicios = async (idMascota, body) => {
-    await MascotasModelo.findByIdAndUpdate({_id:idMascota}, body)
+    await MascotasModelo.findByIdAndUpdate({_id:idMascota, usuario: idUsuario}, body)
     return{
         msg: "Mascota actualizada",
         statusCode: 200
     }
 }
 
-const eliminarMascotaPorIdServicios = async (idMascota) => {
-    await MascotasModelo.findByIdAndDelete({_id: idMascota})
+const eliminarMascotaPorIdServicios = async (idMascota, idUsuario) => {
+    await MascotasModelo.findByIdAndDelete({_id: idMascota, usuario: idUsuario})
     return{
         msg: "Mascota eliminada",
         statusCode: 200
