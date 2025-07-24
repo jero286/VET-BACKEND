711d8ff0f81769ebd95bbd525b7cf92ce2d53f36
unknown <Jernimo>
2025-07-24 15:53:42 -0300

    se soluciona la subida de imagenes

--- src/controladores/productos.controlador.js
+++ src/controladores/productos.controlador.js
@@ -1,60 +1,71 @@
-const { obtenerTodosLosProductosServicios,
-        obtenerProductoPorIdServicios,
-        crearNuevoProductoServicios,
-        actualizarProductoPorIdServicios,
-        eliminarProductoPorIdServicios,
-        cambiarEstadoDeLProductoServicios, } = require("../servicios/productos.servicios")
+const {
+  obtenerTodosLosProductosServicios,
+  obtenerProductoPorIdServicios,
+  crearNuevoProductoServicios,
+  actualizarProductoPorIdServicios,
+  eliminarProductoPorIdServicios,
+  cambiarEstadoDeLProductoServicios,
+} = require("../servicios/productos.servicios");
 
 const obtenerTodosLosProductos = async (req, res) => {
-    const {productos, statusCode} = await obtenerTodosLosProductosServicios()
-    res.status(statusCode).json({productos})
-}
+  const { productos, statusCode } = await obtenerTodosLosProductosServicios();
+  res.status(statusCode).json({ productos });
+};
 
 const obtenerProductoPorId = async (req, res) => {
-    const {producto, statusCode} = await obtenerProductoPorIdServicios(req.params.id)
-    res.status(statusCode).json({producto})
-}
+  const { producto, statusCode } = await obtenerProductoPorIdServicios(
+    req.params.id
+  );
+  res.status(statusCode).json({ producto });
+};
 
 const crearNuevoProducto = async (req, res) => {
-    const {msg, statusCode, idProducto} = await crearNuevoProductoServicios(
-        req.body,
-        req.file
-    )
-    res.status(statusCode).json({msg, idProducto})
-}
+  try {
+     if (!req.file) {
+       return res.status(400).json({ msg: "La imagen es obligatoria" });
+     }
+    const { msg, statusCode, idProducto } = await crearNuevoProductoServicios(
+      req.body,
+      req.file
+    );
+    res.status(statusCode).json({ msg, idProducto });
+  } catch {
+    res.status(400).json({msg:"Verifique bien los campos"})
+  }
+};
 
 const actualizarProductoPorId = async (req, res) => {
-    const {msg, statusCode} = await actualizarProductoPorIdServicios(
-        req.params.id,
-        req.body
-    )
-    res.status(statusCode).json({msg})
-}
+  const { msg, statusCode } = await actualizarProductoPorIdServicios(
+    req.params.id,
+    req.body
+  );
+  res.status(statusCode).json({ msg });
+};
 
-const eliminarProductoPorId = async(req, res) => {
-    const {msg, statusCode} = await eliminarProductoPorIdServicios(
-        req.params.id
-    )
-    res.status(statusCode).json({msg})
-}
+const eliminarProductoPorId = async (req, res) => {
+  const { msg, statusCode } = await eliminarProductoPorIdServicios(
+    req.params.id
+  );
+  res.status(statusCode).json({ msg });
+};
 
-const cambiarEstadoDelProducto = async(req, res) => {
-    const {msg, statusCode, error} = await cambiarEstadoDeLProductoServicios(
-        req.params.idProducto
-    )
+const cambiarEstadoDelProducto = async (req, res) => {
+  const { msg, statusCode, error } = await cambiarEstadoDeLProductoServicios(
+    req.params.idProducto
+  );
 
-    try {
-        res.status(statusCode).json({msg})
-    } catch (error) {
-        res.status(statusCode).json({error})
-    }
-}
+  try {
+    res.status(statusCode).json({ msg });
+  } catch (error) {
+    res.status(statusCode).json({ error });
+  }
+};
 
 module.exports = {
-    obtenerTodosLosProductos,
-    obtenerProductoPorId,
-    crearNuevoProducto,
-    actualizarProductoPorId,
-    eliminarProductoPorId,
-    cambiarEstadoDelProducto
-}
+  obtenerTodosLosProductos,
+  obtenerProductoPorId,
+  crearNuevoProducto,
+  actualizarProductoPorId,
+  eliminarProductoPorId,
+  cambiarEstadoDelProducto,
+};
