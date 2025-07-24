859ca7bd69b35d07b389adb1d54e65f0a45fe640
Magali Canelo <maguidiaz1e@gmail.com>
2025-07-24 11:16:47 -0300

    correccion de config

--- src/mongoDB_config/config.js
+++ src/mongoDB_config/config.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 
 mongoose
-  .connect(process.env.MONGO_PASS)
+  .connect(process.env.MONGO_URI)
   .then(() => console.log("Base de datos conectada"))
   .catch((error) => console.log(error));
