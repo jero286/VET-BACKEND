2299ba8a8e4180dfc86f988968e4ba73f1ce4345
unknown <Jernimo>
2025-08-30 21:48:25 -0300

    se agrega url de netlify en cors con variable de entorno

--- src/helpers/cors.dominioFrontend.js
+++ src/helpers/cors.dominioFrontend.js
@@ -1,5 +1,5 @@
 const corsOptions = {
-  origin: ["http://localhost:5173", "vetfrontend.netlify.app"],
+  origin: ["veterinariacare.netlify.app"],
   credentials: true,
   allowedHeaders: ["Content-Type", "Authorization", "auth", "x-access-token"],
 };
